4003ef8cc09263d9b9bbd751b6aed3234e530158
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-01-03 09:20:13 -0700

    Completes Iteration 1
    Includes Actvity#name, Activity#participants, Activity#total_cost and corresponding tests

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,17 @@
 class Activity
+  attr_reader :name,
+              :participants
+
+  def initialize(name, participants = {})
+    @name         = name
+    @participants = participants
+  end
+
+  def total_cost
+    sum = 0
+    participants.each do |name, amount|
+      sum += amount
+    end
+    sum
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,41 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_has_name
+    activity = Activity.new('hiking')
+
+    assert_instance_of Activity, activity
+    assert_equal 'hiking', activity.name
+  end
+
+  def test_it_has_participants
+    activity = Activity.new('hiking', {"Megan" => 5,
+      "Molly" => 5,
+      "Connor" => 0})
+
+    assert_equal 5, activity.participants["Molly"]
+    assert_equal 3, activity.participants.count
+  end
+
+  def test_participants_can_be_added_to_activities
+    activity = Activity.new('hiking', {"Megan" => 4,
+      "Molly" => 5,
+      "Connor" => 0})
+
+    activity.participants["Andy"] = 6
+
+    assert_equal 4, activity.participants.count
+    assert_equal 6, activity.participants["Andy"]
+  end
+
+  def test_activity_has_total_cost
+    activity = Activity.new('hiking', {"Megan" => 4,
+      "Molly" => 5,
+      "Connor" => 0})
+
+    cost = activity.total_cost
+
+    assert_equal 9, cost
+  end
 end
